c080395d600502c42c43c8b086d62967269f1602
Hugo Gonzalez <hugo.gonzalez.h@outlook.com>
2021-06-21 17:52:01 -0500

    export author repository

--- src/modules/author/author.module.ts
+++ src/modules/author/author.module.ts
@@ -1,8 +1,11 @@
 import { AuthorRepository } from './author.respository'
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { AuthorExistsRule } from './rules/author-exists.rule'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AuthorRepository])]
+  imports: [TypeOrmModule.forFeature([AuthorRepository])],
+  providers: [AuthorExistsRule],
+  exports: [AuthorExistsRule, TypeOrmModule]
 })
 export class AuthorModule {}

4cd1b95e3e69ab1ec5672d37f3f3c59d8c6a1d75
Hugo Gonzalez <hugo.gonzalez.h@outlook.com>
2021-06-21 14:26:51 -0500

    add missing providers to book module

--- src/modules/book/book.module.ts
+++ src/modules/book/book.module.ts
@@ -1,8 +1,13 @@
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Module } from '@nestjs/common'
 import { BookRepository } from './book.repository'
+import { BookService } from './book.service'
+import { BookController } from './book.controller'
+import { BookExistsRule } from './rules/book-exists.rule'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BookRepository])]
+  imports: [TypeOrmModule.forFeature([BookRepository])],
+  providers: [BookService, BookExistsRule],
+  controllers: [BookController]
 })
 export class BookModule {}
